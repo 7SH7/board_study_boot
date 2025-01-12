55de49b23ffb49e2341fb681659028a5adc955ca
7SH7 <kimseunghwan7777@gmail.com>
2025-01-13 01:11:21 +0900

    feat: add relationship onetomany

--- src/main/java/com/example/board_study_with_back_friend/domain/Article.java
+++ src/main/java/com/example/board_study_with_back_friend/domain/Article.java
@@ -2,14 +2,13 @@ package com.example.board_study_with_back_friend.domain;
 
 import com.example.board_study_with_back_friend.controller.request.ArticleRequest;
 import com.example.board_study_with_back_friend.dto.ArticleDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -19,11 +18,19 @@ import lombok.NoArgsConstructor;
 public class Article {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "article_id")
 	private Long id;
 
 	private String title;
 
 	private String content;
+
+	@OneToMany(
+			mappedBy = "article",
+			fetch = FetchType.LAZY,
+			cascade = CascadeType.ALL,
+			orphanRemoval = true)
+	private List<Comment> comments;
 
 	public static Article from(ArticleRequest articleRequest){
 		return Article.builder()
